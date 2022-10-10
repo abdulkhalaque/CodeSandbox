0921cfef424198a38063d6c5bd77a302218dec32
abdulkhalaque <113029498+abdulkhalaque@users.noreply.github.com>
2022-10-10 20:31:52 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Today I start Coding in Sandbox for Interview</h2>
+    </div>
+  );
+}
